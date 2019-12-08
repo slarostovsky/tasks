5f376a25790fa28fb9bdd1a9f5c1ae0acec08148
Slava <slarostovsky@gmail.com>
2019-12-08 12:02:10 +0300

    6-1

---
+++ js/task 6_1 .js
@@ -0,0 +1,35 @@
+let arr = [-1, 10, -9, 5, 6, -10];
+let max = [];
+let total = [];
+
+for (let i = 0; i < arr.length - 1; i++) {
+    let totalMax = arr.reduce((acc, item) => {
+        total.push(acc);
+        return acc += item;
+    }, 0);
+    max.push(Math.max.apply(null, total));
+    delete arr[i]
+}
+let a = Math.max.apply(null, total);
+console.log(a);
+
+
+
+
+
+
+// for (let i = 0; i < arr.length - 1; i++) {
+//     sum = +(arr[i] + arr[i + 1]);
+//     max.push(sum);
+// }
+
+// let a = Math.max.apply(null, max);
+// console.log(a);
+
+
+// const total = arr.reduce((acc, number) => {
+//console.log(acc);
+//     return acc += number;
+// });
+
+// console.log(total)
